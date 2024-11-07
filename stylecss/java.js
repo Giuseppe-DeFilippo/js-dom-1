09a92fa7c366db49f4458554645115948b08ee01
giuse <giuseppedefilippo03@gmail.com>
2024-11-07 15:02:06 +0100

    end

--- stylecss/java.js
+++ stylecss/java.js
@@ -7,9 +7,6 @@ addEventListener('click', function change(){
     
     const img1 = document.getElementById("immage1");
     const img2 = document.getElementById("immage2");
-    const bottone = document.getElementById("bottone")
-
-    bottone.textContent = "spegni";
     img1.src =  "img/yellow_lamp.png";
     
 })
