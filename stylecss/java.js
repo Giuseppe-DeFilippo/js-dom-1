b9d41db77429c8f0f5137bba438bf0735e0b39f8
giuse <giuseppedefilippo03@gmail.com>
2024-11-07 15:06:17 +0100

    start bonus

--- stylecss/java.js
+++ stylecss/java.js
@@ -10,3 +10,14 @@ addEventListener('click', function change(){
     img1.src =  "img/yellow_lamp.png";
     
 })
+
+/*const button = document.getElementById("bottone");
+addEventListener('click', function change(){
+    
+    const img1 = document.getElementById("immage1");
+    const img2 = document.getElementById("immage2");
+    if(img1===lampada spenta){
+
+    }
+    
+})*/
